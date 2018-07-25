4900787f46a1aa2b9ce7513e70cf7cb439c2cf59
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 16:45:36 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,8 +1,9 @@
 def simple_curfew_checker(time)
-  if time >11
+  if time > 11
     "You're in trouble! Better get home quick!"
-  else time == 11
+  elsif time == 11
    "You're in trouble! Better get home quick!" 
+ else
   end
 end
 
